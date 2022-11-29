7da41ffeb55034f1465412c21187fe82a6ec0128
Bogomila <bkatsarska@abv.bg>
2022-11-29 15:04:04 +0200

    Commit based on 'Forms - Part 1' lecture - SoftUni.

--- cycling/cyclist/forms.py
+++ cycling/cyclist/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
 
-class CyclistForm(forms.ModelForm):
-    pass
+class CyclistForm(forms.Form):
+    your_name = forms.CharField(
+        max_length=30,
+        label='Name',
+        help_text="Enter your name",
+    )
+    age = forms.IntegerField(
+        required=False,
+        initial=0,
+    )
